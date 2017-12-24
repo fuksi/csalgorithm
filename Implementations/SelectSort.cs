47dc15618575b032f312b40acc780605c60931cb
Phuc Tran <Phuc.Tran@nordpoolgroup.com>
2017-12-24 13:43:51 +0200

    Init solution with SelectSort including xunit tests

---
+++ Implementations/SelectSort.cs
@@ -0,0 +1,27 @@
+﻿namespace Implementations
+{
+    using System.Collections.Generic;
+
+    public static class SelectSort
+    {
+        public static List<int> Sort(List<int> input)
+        {
+            for (var i = 0; i < input.Count - 1; i++)
+            {
+                var imin = i;
+                for (var j = i + 1; j < input.Count; j++)
+                {
+                    if (input[j] < input[imin])
+                    {
+                        imin = j;
+                    }
+                }
+
+                var min = input[imin];
+                input[imin] = input[i];
+                input[i] = min;
+            }
+            return input;
+        }
+    }
+}
---
+++ Tests/SelectSortTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Tests {
+    using System.Collections.Generic;
+    using Implementations;
+    using Xunit;
+
+    public class SelectSortTests
+    {
+        [Fact]
+        public void SelectSort_UnorderedList_Sorted()
+        {
+            var result = SelectSort.Sort(new List<int> {0, 10, -5, 4, 2, 8});
+            Assert.Equal(new List<int> {-5, 0, 2, 4, 8 ,10}, result);
+        }
+
+        [Fact]
+        public void SelectSort_ShortList_Sorted()
+        {
+            var result = SelectSort.Sort(new List<int> {10, -10});
+            Assert.Equal(new List<int> {-10, 10}, result);
+        }
+
+        [Fact]
+        public void SelectSort_EmptyList_Sorted()
+        {
+            var result = SelectSort.Sort(new List<int> ());
+            Assert.Empty(result);
+        }
+    }
+}
